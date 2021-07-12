dda88f11305008e520dcd5c9018109078a088d4a
Rahshon <tuf70447@temple.edu>
2021-07-12 02:29:19 -0700

    Updated sketch. Using a series of function calls, Car now drives forward, completes a 360 degree right turn, drives forward to return to (roughly) return to its initial position, then moves backwards before starting again. Needs future calibration in order to return to its exact original spot

--- motion.ino
+++ motion.ino
@@ -1,5 +1,6 @@
 /*
--Basic sketch that adds motion to the robotic smart car 
+- Basic sketch that adds motion to the robotic smart car 
+- Using a series of function calls, the car will drive forward, complete a 360 degree right turn, and return (roughly) to its initial position 
 */
 
 int channelA = 5;       //use to enable the motors for both left wheels
@@ -9,6 +10,13 @@ int ltWheelOne = 7;
 int ltWheelTwo = 8;
 int rtWheelOne = 9;
 int rtWheelTwo = 11;
+
+/*Will not work, due to conversion issues. HIGH/LOW = uint ? 
+String active = HIGH;
+String inactive = LOW;
+*/
+
+int delayTime = 1000;
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,51 +34,62 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /*Car Forward*/
+  //Car drives forward
+  moveForward(delayTime);
+  stopCar(delayTime);
+
+  //Car turns right
+  turnRight(delayTime);
+  stopCar(delayTime);
+
+  //car drives forward, returning to its original position 
+  moveForward(delayTime);
+  stopCar(delayTime);
+
+  //Car backs up
+  moveBackward(delayTime);
+  stopCar(delayTime);
+}
+
+
+/* FUNCTIONS */
+void moveForward(int delayTime){
   digitalWrite(ltWheelOne, HIGH);
   digitalWrite(ltWheelTwo, LOW);
   digitalWrite(rtWheelOne, LOW);
   digitalWrite(rtWheelTwo, HIGH);
-  delay(1000);
+  delay(delayTime);
+}
 
-  /*Stop*/
-  digitalWrite(ltWheelOne, LOW);
-  digitalWrite(ltWheelTwo, LOW);
-  digitalWrite(rtWheelOne, LOW);
-  digitalWrite(rtWheelTwo, LOW);
-  delay(1000);
-  
-
-
-  /*Car backward*/
+void moveBackward(int delayTime){
   digitalWrite(ltWheelOne, LOW);
   digitalWrite(ltWheelTwo, HIGH);
   digitalWrite(rtWheelOne, HIGH);
   digitalWrite(rtWheelTwo, LOW);
-  delay(1000);                        /* if stop is not employed after delay the car will keep traveling in this direction for the duration of the delay time */
+  delay(delayTime);
+}
 
-   /*Stop*/
-  digitalWrite(ltWheelOne, LOW);
-  digitalWrite(ltWheelTwo, LOW);
-  digitalWrite(rtWheelOne, LOW);
-  digitalWrite(rtWheelTwo, LOW);
-  delay(1000);
-
-  /*Car Right*/
+void turnRight(int delayTime){
   digitalWrite(ltWheelOne, HIGH);
   digitalWrite(ltWheelTwo, LOW);
   digitalWrite(rtWheelOne, HIGH);
   digitalWrite(rtWheelTwo, LOW);
-  delay(1000);
+  delay(delayTime);
+}
 
-  /*Car left*/
+
+void turnLeft(int delayTime){
   digitalWrite(ltWheelOne, LOW);
   digitalWrite(ltWheelTwo, HIGH);
   digitalWrite(rtWheelOne, LOW);
   digitalWrite(rtWheelTwo, HIGH);
-  delay(4000);
-  
+  delay(delayTime);
+}
 
-  
-
+void stopCar(int delayTime){
+  digitalWrite(ltWheelOne, LOW);
+  digitalWrite(ltWheelTwo, LOW);
+  digitalWrite(rtWheelOne, LOW);
+  digitalWrite(rtWheelTwo, LOW);
+  delay(delayTime);
 }
